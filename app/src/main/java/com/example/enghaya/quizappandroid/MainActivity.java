2bac0ba1647324a87042a5b7180bd602039b879a
ENG.HAYA <haya05055665959>
2017-08-09 07:17:44 +0300

    score

--- app/src/main/java/com/example/enghaya/quizappandroid/MainActivity.java
+++ app/src/main/java/com/example/enghaya/quizappandroid/MainActivity.java
@@ -44,78 +44,74 @@ public class MainActivity extends AppCompatActivity {
         int ans5_score5;
         int final_score;
 //Checking the score for question 1
-
         String answer8;
         Answer = (EditText) this.findViewById(R.id.Answer);
         answer8 = Answer.getText().toString().toLowerCase();
         if (answer8.equals("android")) {
             ans2_score2 = 1;
         } else {
-            ans2_score2= 0;
+            ans2_score2 = 0;
         }
 //Checking the score for question 2
-            Boolean answer2;
-            Answer2 = (RadioButton) this.findViewById(R.id.answer2);
-            answer2 = Answer2.isChecked();
-            if (answer2) {
-                ans1_score1 = 1;
-            } else {
-                ans1_score1 = 0;
-            }
+        Boolean answer2;
+        Answer2 = (RadioButton) this.findViewById(R.id.answer2);
+        answer2 = Answer2.isChecked();
+        if (answer2) {
+            ans1_score1 = 1;
+        } else {
+            ans1_score1 = 0;
+        }
         //Checking the score for question 3
-            Boolean answer5;
-            Answer5 = (RadioButton) this.findViewById(R.id.answer4);
-            answer5 = Answer5.isChecked();//correct
-            if (answer5) {
-                ans4_score4 = 1;
-            } else {
-                ans4_score4 = 0;
-
-            }
+        Boolean answer5;
+        Answer5 = (RadioButton) this.findViewById(R.id.answer4);
+        answer5 = Answer5.isChecked();//correct
+        if (answer5) {
+            ans4_score4 = 1;
+        } else {
+            ans4_score4 = 0;
+        }
         //Checking the score for question 4
-            Boolean answer3_choice1;
-        boolean  answer3_choice2 ;
+        Boolean answer3_choice1;
+        boolean answer3_choice2;
         checkanswer1 = (CheckBox) this.findViewById(R.id.checkanswer1);
-            checkanswer2 = (CheckBox) this.findViewById(R.id.checkanswer2);
-            checkanswer3 = (CheckBox) this.findViewById(R.id.checkanswer3);
-            answer3_choice1 = checkanswer1.isChecked();//correct
+        checkanswer2 = (CheckBox) this.findViewById(R.id.checkanswer2);
+        checkanswer3 = (CheckBox) this.findViewById(R.id.checkanswer3);
+        answer3_choice1 = checkanswer1.isChecked();//correct
         answer3_choice2 = checkanswer2.isChecked();//correct
 
-
-        if (answer3_choice1 ||  answer3_choice2  ) {
-                ans3_score3 = 1;
-            } else {
-                ans3_score3 = 0;
-            }
+        if ((answer3_choice1 || answer3_choice2) && !answer3_choice2) {
+            ans3_score3 = 1;
+        } else {
+            ans3_score3 = 0;
+        }
         //Checking the score for question 5
         Boolean answer7_choice1;
         Boolean answer7_choice2;
         Boolean answer7_choice3;
-         checkanswer4 = (CheckBox) this.findViewById(R.id.checkanswer4);
+        checkanswer4 = (CheckBox) this.findViewById(R.id.checkanswer4);
         checkanswer5 = (CheckBox) this.findViewById(R.id.checkanswer5);
         checkanswer6 = (CheckBox) this.findViewById(R.id.checkanswer6);
-         answer7_choice1 = checkanswer4.isChecked();
+        answer7_choice1 = checkanswer4.isChecked();
         answer7_choice2 = checkanswer5.isChecked();
         answer7_choice3 = checkanswer6.isChecked();
-         if (answer7_choice1 || answer7_choice2 && ! answer7_choice3 ) {
-             ans5_score5 = 1;
+        if (answer7_choice1 || answer7_choice2 && !answer7_choice3) {
+            ans5_score5 = 1;
         } else {
-             ans5_score5 = 0;
+            ans5_score5 = 0;
         }
-         //   displayScore
-            final_score = ans1_score1  + ans3_score3 + ans4_score4 + ans5_score5 + ans2_score2;
+        //   displayScore
+        final_score = ans1_score1 + ans3_score3 + ans4_score4 + ans5_score5 + ans2_score2 + ans2_score2;
 
-            if (final_score == 5) {
-                resultsDisplay = "Good! You scored 5 out of 5";
-            } else {
-                resultsDisplay = " score " + final_score + " out of 5";
-                Context context = getApplicationContext();
-                int duration = Toast.LENGTH_LONG;
-                Toast toast = Toast.makeText(context, resultsDisplay, duration);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-            }
-
-
+        if (final_score == 5) {
+            resultsDisplay = "Good! You scored 5 out of 5";
+        } else {
+            resultsDisplay = " score " + final_score + " out of 5";
         }
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, resultsDisplay, duration);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
+}
